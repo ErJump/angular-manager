86ce5744b227af1615954f2dc81a496c55eb0757
ErJump <fedelijunior@gmail.com>
2023-01-27 10:49:19 +0100

    feat: :sparkles: Adds content in app and adds component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
+import { UserService } from './users.service';
+import { UsersComponent } from './users/users.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
