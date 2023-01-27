a9f0d8513a4028b5763c497405f41fc7949d2fff
ErJump <fedelijunior@gmail.com>
2023-01-27 11:28:21 +0100

    feat: :sparkles: Adds delete user method in users service

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -4,44 +4,49 @@ import { Injectable } from '@angular/core';
     providedIn: 'root'
 })
 export class UserService {
-    getUsers(){
-        return [
-            {
-              name: 'Hidran1',
-              lastname: 'Arias',
-              email: 'hidran@gmail.com',
-              fiscalcode: 'RSAHRN72M22Z444S',
-              province: 'Torino',
-              phone: '454545455',
-              age: 43
-            },
-            {
-              name: 'Hidran2',
-              lastname: 'Arias',
-              email: 'hidran@gmail.com',
-              fiscalcode: 'RSAHRN72M22Z444S',
-              province: 'Torino',
-              phone: '454545455',
-              age: 43
-            },
-            {
-              name: 'Hidran3',
-              lastname: 'Arias',
-              email: 'hidran@gmail.com',
-              fiscalcode: 'RSAHRN72M22Z444S',
-              province: 'Torino',
-              phone: '454545455',
-              age: 43
-            },
-            {
-              name: 'Hidran4',
-              lastname: 'Arias',
-              email: 'hidran@gmail.com',
-              fiscalcode: 'RSAHRN72M22Z444S',
-              province: 'Torino',
-              phone: '454545455',
-              age: 43
-            }
-        ];
+  users = [
+    {
+      name: 'Hidran1',
+      lastname: 'Arias',
+      email: 'hidran@gmail.com',
+      fiscalcode: 'RSAHRN72M22Z444S',
+      province: 'Torino',
+      phone: '454545455',
+      age: 43
+    },
+    {
+      name: 'Hidran2',
+      lastname: 'Arias',
+      email: 'hidran@gmail.com',
+      fiscalcode: 'RSAHRN72M22Z444S',
+      province: 'Torino',
+      phone: '454545455',
+      age: 43
+    },
+    {
+      name: 'Hidran3',
+      lastname: 'Arias',
+      email: 'hidran@gmail.com',
+      fiscalcode: 'RSAHRN72M22Z444S',
+      province: 'Torino',
+      phone: '454545455',
+      age: 43
+    },
+    {
+      name: 'Hidran4',
+      lastname: 'Arias',
+      email: 'hidran@gmail.com',
+      fiscalcode: 'RSAHRN72M22Z444S',
+      province: 'Torino',
+      phone: '454545455',
+      age: 43
     }
+  ]
+  getUsers(){
+    return this.users;
+  }
+  deleteUser(user : any){
+    const index = this.users.indexOf(user);
+    this.users.splice(index, 1);
+  }
 }
