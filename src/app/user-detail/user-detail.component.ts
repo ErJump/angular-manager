cc5cfb9f75fdd22c582a6262b91864099f1d00cc
ErJump <fedelijunior@gmail.com>
2023-01-27 15:12:00 +0100

    feat: :sparkles: Completes reset feature

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -8,13 +8,28 @@ import { User } from '../classes/User';
   styleUrls: ['./user-detail.component.scss']
 })
 export class UserDetailComponent {
-  @Input() user : User | any;
+  private userCopy: User | any;
+  private __user: User | any;
+  @Input() set user(user: User | any) {
+    this.__user = user;
+    this.userCopy = {...user};
+  }
+  get user() {
+    return this.__user;
+  }
   constructor(private uS: UserService) { }
   saveUser() {
     if (this.user.id) {
       this.uS.updateUser(this.user);
     } else {
       this.uS.addUser(this.user);
+    }
+  }
+  resetForm() {
+    if (!this.user.id) {
+      this.user = new User();
+    } else {
+      this.user = this.userCopy;
     }
   }
 }
